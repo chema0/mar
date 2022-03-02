d7fad7e114be87ebf1653f5746bc79390269f2b7
chema <josemariamartinezmanzanares@gmail.com>
2022-03-02 20:01:05 +0100

    Added logging and extended schema

---
+++ mar-api-graphql/src/main/java/mar/instrumentation/CustomInstrumentation.java
@@ -0,0 +1,48 @@
+package mar.instrumentation;
+
+import graphql.ExecutionResult;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.SimpleInstrumentationContext;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomInstrumentation extends SimpleInstrumentation {
+
+    @Override
+    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
+        // Ignoring introspection queries, not relevant
+        String operation = parameters.getOperation();
+        final boolean logging = !operation.equals("IntrospectionQuery");
+
+        long startNanos = System.nanoTime();
+        var executionId = parameters.getExecutionInput().getExecutionId();
+
+        if (logging) {
+            log.info("{}, query:\n {}\n with variables: {}", executionId, parameters.getQuery(), parameters.getVariables());
+        }
+
+        return new SimpleInstrumentationContext<ExecutionResult>() {
+            @Override
+            public void onCompleted(ExecutionResult result, Throwable t) {
+                long duration = TimeUnit.MILLISECONDS.convert(System.nanoTime() - startNanos, TimeUnit.NANOSECONDS);
+
+                if (!logging) return;
+
+                if (t == null) {
+                    log.info("{} completed in: {} ms", executionId, duration);
+                } else {
+                    log.error("{}: failed in: {} ms", executionId, duration, t);
+                }
+            }
+        };
+    }
+}
--- mar-api-graphql/src/main/java/mar/resolvers/QueryResolver.java
+++ mar-api-graphql/src/main/java/mar/resolvers/QueryResolver.java
@@ -4,26 +4,21 @@ import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.RequiredArgsConstructor;
 import mar.mongodb.beans.Model;
 import mar.mongodb.beans.Type;
-import mar.mongodb.repository.ModelRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import mar.mongodb.services.ModelService;
 import org.springframework.stereotype.Component;
-
-import java.util.LinkedList;
 
 @Component
 @RequiredArgsConstructor
 public class QueryResolver implements GraphQLQueryResolver {
 
-    private final ModelRepository modelRepository;
+    private final ModelService modelService;
 
     public Model model(String id) {
-        return modelRepository.findByModelId(id);
+        return modelService.findModelByModelId(id);
     }
 
     public Iterable<Model> models(int limit, Type type) {
-        Pageable pageable = PageRequest.of(1, limit);
-        return modelRepository.findByType(type, pageable);
+        return modelService.findModelsByType(limit, type);
     }
 
 }
